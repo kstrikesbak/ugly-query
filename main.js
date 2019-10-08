40ec18b61b4db629f634e41e11700b3de3338365
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-10-08 15:31:23 -0400

    title adjusted

---
+++ main.js
@@ -0,0 +1,9 @@
+// document.querySelector('h3')
+
+const newTitle = document.querySelector('h1');
+newTitle.style.fontSize = '400px';
+newTitle.style.color = 'red';
+newTitle.style.background = 'url("https://fsmedia.imgix.net/90/c6/de/0a/9038/4001/893f/16b6aeed38e9/2jpg.jpeg?auto=format%2Ccompress&dpr=2&w=650")'
+newTitle.style.backgroundSize = 'cover'
+
+const newParagraph = document.querySelector('')
