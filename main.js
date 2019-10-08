3b06761b305e6816c550ab63b5c14ff9f28defa1
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-10-08 15:57:53 -0400

    background adjusted

--- main.js
+++ main.js
@@ -6,4 +6,10 @@ newTitle.style.color = 'red';
 newTitle.style.background = 'url("https://fsmedia.imgix.net/90/c6/de/0a/9038/4001/893f/16b6aeed38e9/2jpg.jpeg?auto=format%2Ccompress&dpr=2&w=650")'
 newTitle.style.backgroundSize = 'cover'
 
-const newParagraph = document.querySelector('')
+const orderedList = document.querySelector('ol');
+orderedList.style.color = 'blue';
+orderedList.style.fontSize = '300px';
+orderedList.style.background = 'url("https://comps.gograph.com/cartoon-tired-ugly-chipmunk_gg75521345.jpg")'
+
+const allBackgroud = document.querySelector('body')
+allBackgroud.style.background = 'url("https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8xNDA3NjYwMS85ODB4LnBuZyIsImV4cGlyZXNfYXQiOjE1NzEwNjI2MTB9.IZDj6P4suOXVoF771oEf5SvOSpdr87zBR1YxX7cf5gs/img.png")'
