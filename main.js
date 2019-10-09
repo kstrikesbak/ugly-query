76e96cf18ea453c723284a8c8c49f938049f7252
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-10-09 00:01:53 -0400

    small headings adjusted

--- main.js
+++ main.js
@@ -35,6 +35,24 @@ const thirdParagraph = document.querySelector("p:first-child");
 thirdParagraph.style.color = 'red';
 thirdParagraph.style.fontSize = '30px';
 
+const list1 = document.querySelector("#list1");
+list1.style.color = 'yellow';
+const list2 = document.querySelector("#list2");
+list2.style.color = 'rebeccapurple';
+
+const smallHeading1 = document.querySelector("#smallheading1")
+smallHeading1.style.fontSize ='50px';
+smallHeading1.style.textDecoration = "underline overline";
+const smallHeading2 = document.querySelector("#smallheading2")
+smallHeading2.style.fontSize ='50px';
+smallHeading2.style.textDecoration = "underline overline";
+const smallHeading3 = document.querySelector("#smallheading3")
+smallHeading3.style.fontSize ='50px';
+smallHeading3.style.textDecoration = "underline overline";
+
+
+
+
 
 
 
