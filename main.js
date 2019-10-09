e87c17d8a1b83c39851726d93796c5b5950dfabe
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-10-08 23:15:44 -0400

    paragraphs adjusted

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// document.querySelector('h3')
 
 const newTitle = document.querySelector('h1');
 newTitle.style.fontSize = '400px';
@@ -7,9 +6,36 @@ newTitle.style.background = 'url("https://fsmedia.imgix.net/90/c6/de/0a/9038/400
 newTitle.style.backgroundSize = 'cover'
 
 const orderedList = document.querySelector('ol');
-orderedList.style.color = 'blue';
-orderedList.style.fontSize = '300px';
+orderedList.style.color = 'black';
+orderedList.style.fontSize = '500px';
 orderedList.style.background = 'url("https://comps.gograph.com/cartoon-tired-ugly-chipmunk_gg75521345.jpg")'
 
-const allBackgroud = document.querySelector('body')
-allBackgroud.style.background = 'url("https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8xNDA3NjYwMS85ODB4LnBuZyIsImV4cGlyZXNfYXQiOjE1NzEwNjI2MTB9.IZDj6P4suOXVoF771oEf5SvOSpdr87zBR1YxX7cf5gs/img.png")'
+const allBackground = document.querySelector('body')
+allBackground.style.background = 'url("https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8xNDA3NjYwMS85ODB4LnBuZyIsImV4cGlyZXNfYXQiOjE1NzEwNjI2MTB9.IZDj6P4suOXVoF771oEf5SvOSpdr87zBR1YxX7cf5gs/img.png")'
+allBackground.style.opacity = 0.8;
+
+const unorderedList = document.querySelector('ul');
+unorderedList.style.color = 'white';
+unorderedList.style.fontSize = '800px';
+unorderedList.style.background = 'url("https://media2.giphy.com/media/KWzzTbkhDvmQU/giphy.gif")';
+
+const newText = document.querySelector('textarea');
+newText.style.color = 'green';
+newText.style.background= 'url("https://pm1.narvii.com/6207/97ed0e3551fa54a808d697c7522df07e2adffbc3_hq.jpg")'
+
+const firstParagraph = document.querySelector('p');
+firstParagraph.style.color = 'blue';
+firstParagraph.style.fontSize = '30px';
+
+const secondParagraph = document.querySelector("p:last-child");
+secondParagraph.style.color = 'green';
+secondParagraph.style.fontSize = '30px';
+
+const thirdParagraph = document.querySelector("p:first-child");
+thirdParagraph.style.color = 'red';
+thirdParagraph.style.fontSize = '30px';
+
+
+
+
+
